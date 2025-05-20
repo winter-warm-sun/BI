7e33d8d1707ee117c66bf0777c9a24379c26b3e1
如风暖阳 <1491788197@qq.com>
2025-05-20 12:53:42 +0800

    fronted

--- src/main/java/com/hjj/lingxibi/constant/ZhiPuAIConstant.java
+++ src/main/java/com/hjj/lingxibi/constant/ZhiPuAIConstant.java
@@ -2,6 +2,6 @@ package com.hjj.lingxibi.constant;
 
 public interface ZhiPuAIConstant {
 
-   String ZHIPU_AI_API_KEY = "您的智谱AI API key";
+   String ZHIPU_AI_API_KEY = "9e7a4b977a0b4e138a2ae4960f4ef978.UtRG1YFIwBBrZ465";
 
 }
